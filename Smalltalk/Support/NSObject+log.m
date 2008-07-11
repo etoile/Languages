22f56a11f27d6e59fc284bfb3f056e64495dc89d
David Chisnall <github@theravensnest.org>
2008-07-11 23:09:25 +0000

    Added missing file.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@3250 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Smalltalk/Support/NSObject+log.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+@implementation NSObject (log)
+- (void) log
+{
+	NSLog(@"%@", [self description]);
+}
+- (void) printOn:(NSFileHandle*)handle
+{
+	[handle writeData:[[self description] dataUsingEncoding:NSUTF8StringEncoding]];
+}
+@end
