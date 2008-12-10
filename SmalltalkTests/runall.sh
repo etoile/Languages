f2cc28bef675aea41ea5241b672f4533c157c92f
David Chisnall <github@theravensnest.org>
2008-12-10 15:45:08 +0000

    Fixed quiet mode.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@4107 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- SmalltalkTests/runall.sh
+++ SmalltalkTests/runall.sh
@@ -14,7 +14,7 @@ done
 
 # run the tests contained in the Test* directories
 for i in Test*; do
-	if [ $QUIET ] ; then
+	if [ 1 -eq $QUIET ] ; then
 		if (sh runtest.sh $i > /dev/null 2>&1) ; then
 			echo -n .
 		else
