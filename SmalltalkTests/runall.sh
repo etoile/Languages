914bb212119a7e0c6256af4d0b376d2d51fd0a8a
David Chisnall <github@theravensnest.org>
2008-12-10 15:54:16 +0000

    Tidied up quiet mode in runall.sh a bit.  Now exit code is the number of test fails, so it can be used in other scripts easily.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@4109 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- SmalltalkTests/runall.sh
+++ SmalltalkTests/runall.sh
@@ -12,15 +12,24 @@ do
     shift
 done
 
+PASSES=0
+FAILS=0
 # run the tests contained in the Test* directories
 for i in Test*; do
 	if [ 1 -eq $QUIET ] ; then
 		if (sh runtest.sh $i > /dev/null 2>&1) ; then
+			PASSES=`expr $PASSES + 1`
 			echo -n .
 		else
+			FAILS=`expr $FAILS + 1`
 			echo -n '{FAIL: '$i'}'
 		fi	
 	else
     	sh runtest.sh $i
 	fi
 done
+if [ 1 -eq $QUIET ] ; then
+	echo
+	echo `expr $PASSES + $FAILS` tests run.  $PASSES passed, $FAILS failed.
+fi
+exit $FAILS
