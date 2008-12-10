b59d10b9de5dcde8cd68b969bf346635e6623032
David Chisnall <github@theravensnest.org>
2008-12-10 20:55:12 +0000

    Added test for NSRange boxing, and removed debugging code.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@4111 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- SmalltalkKit/Support/NSValue+structs.m
+++ SmalltalkKit/Support/NSValue+structs.m
@@ -11,9 +11,7 @@
 }
 + (id) rangeWithLocation:(int)loc length:(int)len
 {
-	NSLog(@"Creating range from %d to %d", loc, len);
 	NSRange r = [[NSValue valueWithRange:NSMakeRange(loc, len)] rangeValue];
-	NSLog(@"Created Range: %d, %d", r.location, r.length);
 	return [NSValue valueWithRange:NSMakeRange(loc, len)];
 }
 @end
