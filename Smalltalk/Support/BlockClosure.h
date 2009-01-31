8916cef9042869fdb0e4d3542e66b3ad704b965d
Nicolas Roard <nicolas@roard.com>
2009-01-31 16:08:53 +0000

    Fix missing /
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@4376 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Smalltalk/Support/BlockClosure.h
+++ Smalltalk/Support/BlockClosure.h
@@ -1,5 +1,5 @@
 #import <EtoileFoundation/EtoileFoundation.h>
-#import <LanguageKitRuntime/BlockClosure.h>
+#import <LanguageKit/Runtime/BlockClosure.h>
 
 @interface BlockClosure (ExceptionHandling)
 - (id) onException:(NSString*)exceptionName do:(BlockClosure*)handler;
