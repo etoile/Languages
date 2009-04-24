e45c53b5ca0261a318d8112522686a027838ea71
David Chisnall <github@theravensnest.org>
2009-04-24 17:44:44 +0000

    Added some documentation to ABI.h.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@4586 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- LanguageKit/CodeGen/ABI.h
+++ LanguageKit/CodeGen/ABI.h
@@ -1,3 +1,18 @@
+/**
+ * ABI.h defines three constants for each target architecture which define the
+ * calling convention for returning structures from functions.  These are:
+ *
+ * MAX_INTS_IN_REGISTERS - This defines the maximum number of integers in a
+ * structure that can be returned in registers.
+ *
+ * MAX_FLOATS_IN_REGISTERS - This defines the maximum number of floating point
+ * values in a structure that can be returned in registers.  
+ *
+ * Any structure with more elements than this will be returned on the stack.
+ * The final value, SMALL_FLOAT_STRUCTS_ON_STACK, is a flag which indicates
+ * whether small structures (e.g. NSPoint) should be returned in integer
+ * registers.  This is the case on FreeBSD/i386, for example.
+ */
 // These are different for x86-64, but I don't know what they should be.
 #ifdef __i386__
 #	ifdef __FreeBSD__
