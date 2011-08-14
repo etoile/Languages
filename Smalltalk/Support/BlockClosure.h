add876f5d91461d08f1c12ee637339df2adc7cf4
David Chisnall <github@theravensnest.org>
2011-08-14 15:36:13 +0000

    Fix catching exceptions from Smalltalk.
    Tweak the Smalltalk GNUmakefile to support parallel build (make sure that smalltalk.h is known not to exist until the parser is generated).
    
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@7246 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Smalltalk/Support/BlockClosure.h
+++
@@ -1,6 +0,0 @@
-#import <EtoileFoundation/EtoileFoundation.h>
-#import <LanguageKitRuntime/BlockClosure.h>
-
-@interface BlockClosure (ExceptionHandling)
-- (id) onException:(NSString*)exceptionName do:(BlockClosure*)handler;
-@end
--- Smalltalk/Support/BlockClosure.m
+++ Smalltalk/Support/BlockClosure.m
@@ -1,27 +1,35 @@
 #import <Foundation/Foundation.h>
-#import "BlockClosure.h"
 
-@implementation BlockClosure (ExceptionHandling)
-- (id) onException: (NSString*) exceptionName do: (BlockClosure*) handler
+extern NSString *LKSmalltalkBlockNonLocalReturnException;
+
+@interface _NSBlock : NSObject
+- (id)value;
+@end
+
+@implementation _NSBlock (ExceptionHandling)
+- (id) onException: (NSString*) exceptionName do: (id(^)(id)) handler
 {
-	NS_DURING
-		NS_VALUERETURN([self value], id);
-	NS_HANDLER
+	@try 
+	{
+		return [self value];
+	}
+	@catch (NSException *localException)
+	{
 		// If this is an exception that the interpreter is using to fudge a
 		// non-local return, chuck it out to the interpreter.
 		if ([[localException name] isEqualToString: LKSmalltalkBlockNonLocalReturnException])
 		{
 			[localException raise];
 		}
-		if ([[localException name] isEqualToString: exceptionName])
+		if ((exceptionName == nil) || [[localException name] isEqualToString: exceptionName])
 		{
-			return [handler value: localException];
+			return handler(localException);
 		}
 		else
 		{
 			[localException raise];
 		}
-	NS_ENDHANDLER
+	}
 	// Not reached
 	return nil;
 }
