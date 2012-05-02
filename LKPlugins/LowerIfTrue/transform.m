b86df99986d5153f60a8ca8317eabdc35fa86498
David Chisnall <github@theravensnest.org>
2012-05-02 13:29:47 +0000

    Remove external references to symbols referenced in a block when removing the block.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@7718 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- LKPlugins/LowerIfTrue/transform.m
+++ LKPlugins/LowerIfTrue/transform.m
@@ -1,4 +1,5 @@
 #import <LanguageKit/LanguageKit.h>
+#import <EtoileFoundation/EtoileFoundation.h>
 
 NSArray *statementsFromBlock(LKAST*aBlock)
 {
@@ -10,35 +11,72 @@ NSArray *statementsFromBlock(LKAST*aBlock)
 	LKMessageSend *valueMsg = [LKMessageSend messageWithSelectorName: @"value"];
 	[valueMsg setTarget: aBlock];
 	return A(valueMsg);
-}	
+}
+@interface LKCollectExternalRefs : LKASTVisitor
+{
+	@public
+	NSMutableSet *refs;
+}
+@end
+@implementation LKCollectExternalRefs
+- (LKAST*)visitDeclRef: (LKDeclRef*)aDeclRef
+{
+	[refs addObject: [[aDeclRef symbol] stringValue]];
+	return aDeclRef;
+}
+@end
 
 @interface LKLowerIfTrueTransform : LKASTVisitor
 @end
 @implementation LKLowerIfTrueTransform 
 - (LKAST*) visitMessageSend:(LKMessageSend*)aNode
 {
-	NSArray *thenClause = nil;
-	NSArray *elseClause = nil;
+	LKBlockExpr *thenBlock = nil;
+	LKBlockExpr *elseBlock = nil;
 	if ([@"ifTrue:ifFalse:" isEqualToString:[aNode selector]])
 	{
 		NSArray *args = [aNode arguments];
-		thenClause = statementsFromBlock([args objectAtIndex:0]);
-		elseClause = statementsFromBlock([args objectAtIndex:1]);
+		thenBlock = [args objectAtIndex:0];
+		elseBlock = [args objectAtIndex:1];
 	}
 	else if ([@"ifTrue:" isEqualToString:[aNode selector]])
 	{
 		NSArray *args = [aNode arguments];
-		thenClause = statementsFromBlock([args objectAtIndex:0]);
+		thenBlock = [args objectAtIndex:0];
 	}
 	else if ([@"ifFalse:" isEqualToString:[aNode selector]])
 	{
 		NSArray *args = [aNode arguments];
-		elseClause = statementsFromBlock([args objectAtIndex:0]);
+		elseBlock = [args objectAtIndex:0];
 	}
 	else 
 	{
 		return aNode;
 	}
+
+	// Remove references to symbols from the blocks we're deleting
+	LKCollectExternalRefs *exts = [LKCollectExternalRefs new];
+	LKSymbolTable *symTab = [aNode symbols];
+	NSMutableSet *refs = [NSMutableSet new];
+	exts->refs = refs;
+	[thenBlock visitWithVisitor: exts];
+	for (NSString *sym in refs)
+	{
+		LKSymbol *s = [symTab symbolForName: sym];
+		s.referencingScopes--;
+	}
+	[refs removeAllObjects];
+	[elseBlock visitWithVisitor: exts];
+	for (NSString *sym in refs)
+	{
+		LKSymbol *s = [symTab symbolForName: sym];
+		s.referencingScopes--;
+	}
+	[refs release];
+	[exts release];
+
+	NSArray *thenClause = statementsFromBlock(thenBlock);
+	NSArray *elseClause = statementsFromBlock(elseBlock);
 	LKAST *receiver = [aNode target];
 	LKMessageSend *condition = 
 		[LKMessageSend messageWithSelectorName: @"boolValue"];
