dec84475b53cce24fff2d072a4558b128f036523
David Chisnall <github@theravensnest.org>
2013-01-31 16:31:32 +0000

    Allow us to keep tracking source locations when concatenating tokens.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages@8010 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- LanguageKit/LKToken.m
+++ LanguageKit/LKToken.m
@@ -3,6 +3,55 @@
 
 typedef unichar(*CIMP)(id, SEL, unsigned);
 
+@implementation NSString (Token)
+- (NSRange) sourceLocation
+{
+	return NSMakeRange(0, 0);
+}
+@end
+
+@interface LKCompositeToken : NSString
+{
+	NSString *str;
+	NSRange range;
+}
+- (id)initWithStart: (NSString*)s end: (NSString*)e;
+@end
+@implementation LKCompositeToken
+- (id)initWithStart: (NSString*)s end: (NSString*)e
+{
+	self = [super init];
+	NSRange r = [e sourceLocation];
+	str = [NSString stringWithFormat: @"%@%@", s, e];
+	range.location = [s sourceLocation].location;
+	range.length = r.location - range.location + r.length;
+	return self;
+}
+- (NSString*)stringByAppendingString: (NSString*)other
+{
+	NSRange secondRange = [other sourceLocation];
+	NSUInteger end = secondRange.location + secondRange.length;
+	if (end < range.location + range.length)
+	{
+		return [super stringByAppendingString: other];
+	}
+	return [[LKCompositeToken alloc] initWithStart: self end: other];
+}
+- (NSUInteger) length
+{
+	return [str length];
+}
+- (unichar) characterAtIndex:(NSUInteger)index
+{
+	return [str characterAtIndex: index];
+}
+- (NSRange) sourceLocation
+{
+	return range;
+}
+@end
+
+
 @implementation LKToken
 - (LKToken*) initWithRange:(NSRange)aRange inSource:(NSString*)aString
 {
@@ -32,5 +81,15 @@ typedef unichar(*CIMP)(id, SEL, unsigned);
 - (NSString*) sourceDocument
 {
 	return source;
+}
+- (NSString*)stringByAppendingString: (NSString*)str
+{
+	NSRange secondRange = [str sourceLocation];
+	NSUInteger end = secondRange.location + secondRange.length;
+	if (end < range.location + range.length)
+	{
+		return [super stringByAppendingString: str];
+	}
+	return [[LKCompositeToken alloc] initWithStart: self end: str];
 }
 @end
